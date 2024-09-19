d6318ed0e57ad0658b2c76702710dbd53d25de89
mdnasiruddin78 <nasiruddinanik54@gmail.com>
2024-09-19 10:30:41 +0600

    add money section add

--- java/home.js
+++ java/home.js
@@ -1,1 +1,15 @@
-console.log('home add asss')
+
+// step-01 add event handler to the add money button inside the from
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    event.preventDefault();
+    // console.log('money add button clicked')
+
+    // step-02 get money to be added to the acount
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+
+    // get the pin number
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(pinNumberInput)
+
+});
