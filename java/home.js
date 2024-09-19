3d4bedf1b97c7293106f463a4502663926191d27
mdnasiruddin78 <nasiruddinanik54@gmail.com>
2024-09-19 11:44:22 +0600

    another section add

--- java/home.js
+++ java/home.js
@@ -5,11 +5,34 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
     // console.log('money add button clicked')
 
     // step-02 get money to be added to the acount
-    const addMoneyInput = document.getElementById('input-add-money').value;
+    const addMoneyInput = document.getElementById("input-add-money").value;
     console.log(addMoneyInput);
 
     // get the pin number
-    const pinNumberInput = document.getElementById('input-pin-number').value;
-    console.log(pinNumberInput)
+    const pinNumberInput = document.getElementById("input-pin-number").value;
+    console.log(typeof pinNumberInput)
+
+    // step-03 verify pin number
+    if(pinNumberInput === '1234'){
+        console.log('add money to your account');
+
+        //step-04 get the current balance
+        const balance = document.getElementById("account-balance").innerText;
+        console.log(typeof balance);
+
+
+        // step-05 sum
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+
+        // step-06 update update the balance in the ui/dom
+        document.getElementById('account-balance').innerText = newBalance
+
+    }
+    else{
+        alert('Failed to add money please try again later')
+    }
 
 });
